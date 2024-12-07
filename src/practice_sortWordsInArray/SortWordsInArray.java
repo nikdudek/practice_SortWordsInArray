9e40ac5e26048acf0fcdcca78752bd1f7ff62cd4
nikdudek <nik.dudek@gmail.com>
2024-12-07 18:49:23 +0100

    Implemented successfully using anonymous function.

--- src/practice_sortWordsInArray/SortWordsInArray.java
+++ src/practice_sortWordsInArray/SortWordsInArray.java
@@ -1,10 +1,26 @@
 package practice_sortWordsInArray;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class SortWordsInArray {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.print("Please, enter words separated by space: ");
+		Scanner sc = new Scanner(System.in);
+		String words = sc.nextLine();
+		String[] arrOfWords = words.split("\\s+");
+		Arrays.sort(arrOfWords, (s1,s2) -> {
+			int result = s2.length() - s1.length();
+			if (result == 0) {
+				result = s1.compareToIgnoreCase(s2);
+			}
+			return result;
+		});
+		for( String word : arrOfWords) {
+			System.out.print(word + " ");
+		}
+		sc.close();
 	}
 
 }
